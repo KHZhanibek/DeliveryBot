30c55494d9d6289aed7d013bb6f7e3a9f05dd139
Nizami Jussupov <80578329+jnzm02@users.noreply.github.com>
2021-11-08 18:30:19 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import telebot
+from telebot import types
+
+bot = telebot.TeleBot('2082841531:AAGGrZ8tdMwcNaximIWu0d9nb-FRcPPOfQo')
+
+@bot.message_handler(commands = ['start'])
+def StartCommand(message):
+	markup_inline = types.ReplyKeyboardMarkup(resize_keyboard = True)
+	
+	item_basket = types.InlineKeyboardButton(text = 'Корзина', callback_data = 'basket')
+	item_my_orders = types.InlineKeyboardButton(text = 'Мои Заказы', callback_data = 'my_orders')
+	item_checkout = types.InlineKeyboardButton(text = 'Оформить Заказ', callback_data = 'checkout')
+
+	markup_inline.add(item_checkout, item_basket, item_my_orders)
+	bot.send_message(message.chat.id, 'Добрый день!',
+		reply_markup=markup_inline
+	)
+
+bot.polling(none_stop = True, interval = 0)
